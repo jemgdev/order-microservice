eb672f3295338db211e383f2457808d7a1f4d059
jemgdev <jemgdev@gmail.com>
2022-11-21 21:04:52 -0500

    asda

--- src/core/order/infrastructure/prisma/order.prisma.repository.ts
+++ src/core/order/infrastructure/prisma/order.prisma.repository.ts
@@ -21,7 +21,7 @@ export default class OrderPrismaRepository implements OrderRepository {
         }
       },
       orderBy: {
-        orderDate: 'asc'
+        orderDate: 'desc'
       }
     })
   }
